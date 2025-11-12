cb8f87925c3c0911edb1f93a0913ca75aced804b
Roman <vendettabay911@gmail.com>
2025-11-12 10:44:08 +0300

    Проблема с запуском тестов

---
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupCreationTests extends TestBase {
+
+
+    @Test
+    public void canCreateGroup() {
+        app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+
+    }
+
+    @Test
+    public void canCreateGroupWithEmptyName() {
+        app.groups().createGroup(new GroupData());
+
+    }
+
+    @Test
+    public void canCreateGroupWithNameOnly() {
+        app.groups().createGroup(new GroupData().withName("some name"));
+
+    }
+}
